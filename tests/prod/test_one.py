eff092ea8b1a72b52886a92953a62bed8c72043f
Katerina Mangileva <ms.katiysha@mail.ru>
2024-03-10 21:15:28 +0500

    init

---
+++ tests/prod/test_one.py
@@ -0,0 +1,23 @@
+import allure
+import pytest
+
+
+class TestDemo:
+
+    @allure.title("Успешный тест")
+    def test_one(self):
+        assert 1 == 1
+
+    @allure.title("Пропущенный тест")
+    @pytest.mark.skip
+    def test_skipped(self):
+        assert 1 == 1
+
+    @allure.title("Тест с ошибкой в коде")
+    def test_failed(self):
+        assert 1 == 2
+
+    @allure.title("Тест с ошибкой в тесте")
+    def test_broken(self):
+        assert 1 == 1
+        raise Exception("Ошибка в тесте")
