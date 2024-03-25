ed5487a19382cb3657e2f88834490ca02b12e0ee
Katerina Mangileva <ms.katiysha@mail.ru>
2024-03-25 12:38:23 +0500

    update

--- tests/adm/test_two.py
+++ tests/adm/test_two.py
@@ -5,12 +5,19 @@ from qaseio.pytest import qase
 
 @qase.title('Второй набор тестов')
 @qase.description('Это фальшивые тесты')
-@allure.feature("Don't simple test")
-@allure.story("existsnce")
-@allure.suite("test_one")
 class TestDemo2:
 
     @allure.title("Успешный тест")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("1111")
+    @allure.suite("ТРЕТИЙ")
     def test_one(self):
         assert 1 == 1
 
@@ -20,10 +27,30 @@ class TestDemo2:
         assert 1 == 1
 
     @allure.title("Тест с ошибкой в коде")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("4444")
+    @allure.suite("ЧЕТВЕРТЫЙ")
     def test_failed(self):
         assert 1 == 2
 
     @allure.title("Тест с ошибкой в тесте")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("1111")
+    @allure.suite("ПЯТЫЙ")
     def test_broken(self):
         assert 1 == 1
         raise Exception("Ошибка в тесте")
--- tests/prod/test_one.py
+++ tests/prod/test_one.py
@@ -5,25 +5,56 @@ from qaseio.pytest import qase
 
 @qase.title('Первый набор тестов')
 @qase.description('Это фальшивые тесты')
-@allure.feature("Simple test")
-@allure.story("1111")
-@allure.suite("ПЕРВЫЙ")
 class TestDemo:
 
     @allure.title("Успешный тест")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("1111")
+    @allure.suite("ПЕРВЫЙ")
     def test_one(self):
-        assert 1 == 1
+        with qase.step("Make assert"):
+            assert 1 == 1
 
     @allure.title("Пропущенный тест")
     @pytest.mark.skip
     def test_skipped(self):
-        assert 1 == 1
+        with qase.step("Make assert"):
+            assert 1 == 1
 
     @allure.title("Тест с ошибкой в коде")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("2222222")
+    @allure.suite("ВТОРОЙ")
     def test_failed(self):
-        assert 1 == 2
+        with qase.step("Make assert"):
+            assert 1 == 2
 
     @allure.title("Тест с ошибкой в тесте")
+    @qase.fields(
+        ("severity", "critical"),
+        ("priority", "high"),
+        ("layer", "unit"),
+        ("description", "Try to login to Qase TestOps using login and password"),
+        ("description", "*Precondition 1*. Markdown is supported."),
+    )
+    @allure.feature("Simple test")
+    @allure.story("1111")
+    @allure.suite("ВТОРОЙ")
     def test_broken(self):
-        assert 1 == 1
-        raise Exception("Ошибка в тесте")
+        with qase.step("Make assert"):
+            assert 1 == 1
+            raise Exception("Ошибка в тесте")
